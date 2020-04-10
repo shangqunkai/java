cfdcc694d22d05c81237a9dde0445fd9c4c0c12d
31716 <31716@DESKTOP-SB7GP7I>
2020-04-10 15:26:15 +0800

    update

--- src/main/java/com/tsdata/model/factor/service/impl/GetAFuShareResultServiceImpl.java
+++ src/main/java/com/tsdata/model/factor/service/impl/GetAFuShareResultServiceImpl.java
@@ -57,24 +57,24 @@ public class GetAFuShareResultServiceImpl implements GetInterfaceInfoService {
 		String idNo = param.getString("id_no");
 
 		Map loanRecord = new HashMap();
-		loanRecord.put("approvalStatus", "");// 审批结果码
-		loanRecord.put("idNo", idNo);// 被查询借款人身份证号
-		loanRecord.put("loanAmount", "");// 借款金额，通过的，取合同金额;未通过或审核中的， 取申请金额
-		loanRecord.put("loanDate", "");// 借款时间，通过的，取合同时间;未通过或审核中的， 取申请时间。作为数据提供方，平台可识别的时间格式有 2 种:YYYYMM或者YYYYMMDD
-		loanRecord.put("loanStatus", "");// 还款状态码，指一笔借款合同当前的状态;若历史出现过逾期，当前还款正常，则还款状态取“正常”
-		loanRecord.put("loanType", "");// 借款类型码，指一笔借款所属的类型
-		loanRecord.put("name", name);// 被查询借款人姓名
-		loanRecord.put("overdueAmount", "");// 逾期金额，指一笔借款中，达到还款期限，尚未偿还 的总金额
-		loanRecord.put("overdueM3", "");// 历史逾期 M3+次数(不含 M3，包括 M6 及以上)
-		loanRecord.put("overdueM6", "");// 历史逾期 M6+次数(不含 M6)
-		loanRecord.put("overdueStatus", "");// 逾期情况，指一笔借款当前逾期的程度
-		loanRecord.put("overdueTotal", "");// 历史逾期总次数
-		loanRecord.put("periods", "");// 期数, , 通过的，取合同期数;未通过或审核中的，取 申请期数，范围1~120
+		loanRecord.put("approvalStatus", "ACCEPT");// 审批结果码
+		loanRecord.put("idNo", "370633197005042513");// 被查询借款人身份证号
+		loanRecord.put("loanAmount", 20);// 借款金额，通过的，取合同金额;未通过或审核中的， 取申请金额
+		loanRecord.put("loanDate", "202003");// 借款时间，通过的，取合同时间;未通过或审核中的， 取申请时间。作为数据提供方，平台可识别的时间格式有 2 种:YYYYMM或者YYYYMMDD
+		loanRecord.put("loanStatus", "OVERDUE");// 还款状态码，指一笔借款合同当前的状态;若历史出现过逾期，当前还款正常，则还款状态取“正常”
+		loanRecord.put("loanType", "CREDIT");// 借款类型码，指一笔借款所属的类型
+		loanRecord.put("name", "测试");// 被查询借款人姓名
+		loanRecord.put("overdueAmount", 20);// 逾期金额，指一笔借款中，达到还款期限，尚未偿还 的总金额
+		loanRecord.put("overdueM3", 1);// 历史逾期 M3+次数(不含 M3，包括 M6 及以上)
+		loanRecord.put("overdueM6", 1);// 历史逾期 M6+次数(不含 M6)
+		loanRecord.put("overdueStatus", "M3+");// 逾期情况，指一笔借款当前逾期的程度
+		loanRecord.put("overdueTotal", 5);// 历史逾期总次数
+		loanRecord.put("periods", 36);// 期数, , 通过的，取合同期数;未通过或审核中的，取 申请期数，范围1~120
 		Map riskResult = new HashMap();// 风险项记录
-		riskResult.put("riskDetail", "");// 风险明细，合作机构提供的借款人的风险类别
-		riskResult.put("riskItemType", "");// 命中项码，如证件号码(当前命中项仅包括证件号码)
-		riskResult.put("riskItemValue", "");// 命中内容，身份证号的具体值
-		riskResult.put("riskTime", "");// 风险最近时间，指风险记录最近一次发现的时间 平台可识别的时间格式有 3 种:YYYY YYYYMM YYYYMMDD
+		riskResult.put("riskDetail", "命中外部黑名单");// 风险明细，合作机构提供的借款人的风险类别
+		riskResult.put("riskItemType", "ID_NO");// 命中项码，如证件号码(当前命中项仅包括证件号码)
+		riskResult.put("riskItemValue", "370633197005042513");// 命中内容，身份证号的具体值
+		riskResult.put("riskTime", "2017");// 风险最近时间，指风险记录最近一次发现的时间 平台可识别的时间格式有 3 种:YYYY YYYYMM YYYYMMDD
 										
 		List<Map> loanRecords = new ArrayList<>();
 		loanRecords.add(loanRecord);
